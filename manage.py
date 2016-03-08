674f29559fb3082710b202eebcc5d36d64428cec
Yadan.Js <y_shirvany@yahoo.com>
2016-03-07 20:56:19 -0500

    wsgi

--- manage.py
+++ manage.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python
 import os
-import sys
 
-if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "userTracker.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "userTracker.settings")
 
-    from django.core.management import execute_from_command_line
+from django.core.wsgi import get_wsgi_application
+from whitenoise.django import DjangoWhiteNoise
 
-    execute_from_command_line(sys.argv)
+application = get_wsgi_application()
+application = DjangoWhiteNoise(application)
