987b51865fd514efd2cbf68bc35f62487d3b7309
Yadan.Js <y_shirvany@yahoo.com>
2016-03-07 20:58:34 -0500

    wsgi

--- manage.py
+++ manage.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python
 import os
+import sys
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "userTracker.settings")
+if __name__ == "__main__":
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "userTracker.settings")
 
-from django.core.wsgi import get_wsgi_application
-from whitenoise.django import DjangoWhiteNoise
+    from django.core.management import execute_from_command_line
 
-application = get_wsgi_application()
-application = DjangoWhiteNoise(application)
+    execute_from_command_line(sys.argv)
--- userTracker/wsgi.py
+++ userTracker/wsgi.py
@@ -1,18 +1,9 @@
-"""
-WSGI config for userTracker project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
-"""
 
 import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "userTracker.settings")
 
 from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "userTracker.settings")
 
 application = get_wsgi_application()
 application = DjangoWhiteNoise(application)
